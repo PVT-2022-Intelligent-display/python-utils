866442b05a056728d7fd7fa3daad53f7ff016e8d
ptrbroz <10211738+ptrbroz@users.noreply.github.com>
2022-05-19 19:15:14 +0200

    make simulator react to celsius/farenheit button

--- externalDataSimulator.py
+++ externalDataSimulator.py
@@ -3,7 +3,7 @@ import time
 from time import sleep
 import random
 
-ser = serial.Serial('COM4', 115200) #replace with your com port
+ser = serial.Serial('COM4', 115200, timeout=1) #replace with your com port
 
 #generates random temperatures. Sends them to display in celsius.
 #When it gets a message from display that button for switching between celsius
@@ -14,11 +14,16 @@ ser = serial.Serial('COM4', 115200) #replace with your com port
 
 stupidAmericanScale = 0
 lastTemp = 25.0
-sleepTime = 1.5
+sleepTime = 0.5
 
 labelId = 5
 
+FLIP_DEBOUNCE_VALUE = 2
+debounceCounter = 0
+
 while True:
+    if(debounceCounter > 0):
+        debounceCounter = debounceCounter - 1
     time.sleep(sleepTime)
     lastTemp = lastTemp + random.uniform(-1.5, 1.5)
     if(lastTemp > 60.0 or lastTemp < 0.1):
@@ -38,3 +43,17 @@ while True:
     ser.write(configStr1.encode())
     ser.write(degree)
     ser.write(configStr2.encode())
+
+    incoming = ser.readline()
+    print(incoming.decode('ascii'))
+    if debounceCounter == 0 and (incoming == b'Type:2 ID:4\n' or incoming == b'\rType:2 ID:4\n'):
+        if stupidAmericanScale:
+            stupidAmericanScale = 0
+        else:
+            stupidAmericanScale = 1
+        print("flip")
+        debounceCounter = FLIP_DEBOUNCE_VALUE
+
+
+    
+    
